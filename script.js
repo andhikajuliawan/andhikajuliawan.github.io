108b1a032558759b3d8c009f005171957ce9013d
aje12345 <andhikajuliawan@student.ittelkom-sby.ac.id>
2021-10-21 09:00:32 +0700

    mengubah script.js dengan fitur prompt

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("hai semuanya")
+let nama = prompt("hai selamat datang kalo boleh tau siapa namamu ?");
+if (typeof nama == "string") {
+    alert("Salam kenal " + nama) 
+} else {
+    console.error ("you didn't write your real name")
+}
