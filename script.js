a9d71e49143750ad95790dd9cb21d10fa16c42d0
andhikajuliawan <andhikajuliawan29@gmail.com>
2022-03-20 13:02:32 +0700

    add artikel

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let nama = prompt("hai selamat datang kalo boleh tau siapa namamu ?");
-if (typeof nama == "string") {
-    alert("Salam kenal " + nama) 
-} else {
-    console.error ("you didn't write your real name")
-}
+// let nama = prompt("hai selamat datang kalo boleh tau siapa namamu ?");
+// if (typeof nama == "string") {
+//     alert("Salam kenal " + nama)
+// } else {
+//     console.error ("you didn't write your real name")
+// }
